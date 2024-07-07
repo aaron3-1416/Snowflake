2dc0d4a0a655463f39b106241241a2704fc2cc1f
aaron3-1416 <92693623+aaron3-1416@users.noreply.github.com>
2024-07-07 14:28:36 -0600

    roles hierarchy

--- Maja Ferle/CHP 01 - Introduction to the Cloud Data Platform/CHP01_code.sql
+++ Maja Ferle/CHP 01 - Introduction to the Cloud Data Platform/CHP01_code.sql
@@ -1,3 +1,15 @@
 use role SYSADMIN;
 create warehouse COMPUTE_WH with warehouse_size = 'X-Small';
 grant usage on warehouse COMPUTE_WH to PUBLIC;
+
+USE ROLE SECURITYADMIN;
+
+CREATE role if not exists webmatch_administrator;
+CREATE role if not exists webmatch_engineer;
+CREATE role if not exists webmatch_analyst;
+GRANT role webmatch_analyst TO role webmatch_engineer;
+GRANT role webmatch_engineer TO role webmatch_administrator;
+
+use database snowflake;
+use schema account_usage;
+CREATE ROLE IF NOT EXISTS ;
